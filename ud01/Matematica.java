3a369f2eb06bbde8be8d7d3fdb2a674575738286
mateopitaguilar <mateooaguilar20@gmail.com>
2024-10-02 10:03:32 +0200

    primeros lenguajes

---
+++ ud01/Matematica.java
@@ -0,0 +1,18 @@
+//Crear programa que:
+// calcule raiz cuadrada de 234324
+// calcule 34 elevado a 17
+
+ /**
+  * Matematica
+  */
+ public class Matematica {
+ 
+    public static void main(String[] args) {
+       
+        double raiz = Math.sqrt(234324);
+        System.out.println("La raiz cuadrada de 234324 es: " + raiz);
+        
+        double potencia = Math.pow(34,17);
+        System.out.println("La potencia de 34 elevado a 17 es: " + potencia);
+    }
+ }
---
+++ ud01/hola.c
@@ -0,0 +1,9 @@
+/* Programa: Hola mundo */
+
+#include <stdio.h>
+
+void main()
+{
+    printf( "Son un developer top\n" );
+    printf( "Son un developer top TOP\n" );
+}   
