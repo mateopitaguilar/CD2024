d5cd2306288ae992f1c9360c4e5b79a6cf277cc8
mateopitaguilar <mateooaguilar20@gmail.com>
2025-03-11 15:55:21 +0100

    Validar contraseña

--- ud03/demo/src/main/java/com/mateopa/ValidadorContraseña.java
+++ ud03/demo/src/main/java/com/mateopa/ValidadorContraseña.java
@@ -1,22 +1,28 @@
 package com.mateopa;
 
 public class ValidadorContraseña {
-    public boolean esValida(String contraseña) {
-        if (contraseña == null || contraseña.length() < 8) {
-            return false;
+
+    public boolean esValida(String contrasinal){
+        if (contrasinal.length() < 8) return false;
+        int cont = 0;
+        for(int i = 0; i < contrasinal.length(); i++){ 
+            if ((contrasinal.charAt(i) >= 'A' && contrasinal.charAt(i) <= 'Z')) cont++;
         }
-        if (!contraseña.matches(".*[A-Z].*")) {
-            return false;
+        if (cont == 0) return false;
+
+        int cont2 = 0;
+        for(int i = 0; i < contrasinal.length(); i++){ 
+            if ((contrasinal.charAt(i) >= 'a' && contrasinal.charAt(i) <= 'z')) cont2++;
         }
-        if (!contraseña.matches(".*[a-z].*")) {
-            return false;
+        if (cont2 == 0) return false;
+
+        int cont3 = 0;
+        for(int i = 0; i < contrasinal.length(); i++){ 
+            if ((contrasinal.charAt(i) >= '0' && contrasinal.charAt(i) <= '9')) cont3++;
         }
-        if (!contraseña.matches(".*\\d.*")) {
-            return false;
-        }
-        if (!contraseña.matches(".*[!@#$%^&*].*")) {
-            return false;
-        }
+        if (cont3 == 0) return false;
+        if (!(contrasinal.matches(".*[!@#$%^&*].*")))return false;
+
         return true;
     }
 }
--- ud03/demo/src/test/java/com/mateopa/ValidadorContraseñaTest.java
+++ ud03/demo/src/test/java/com/mateopa/ValidadorContraseñaTest.java
@@ -1,6 +1,7 @@
 package com.mateopa;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,11 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidadorContraseñaTest {
 
+ValidadorContraseña validador = new ValidadorContraseña();
+
  @Test
 
  void testContraseñaDebeTenerAlMenosOchoCaracteres() {
-
- ValidadorContraseña validador = new ValidadorContraseña();
 
  assertFalse(validador.esValida("Ab1!")); // Demasiado corta
 
